587569cfc2b699f37ef53d43fa332e2d858aa0ad
Chanaka Lakmal <ldclakmal@gmail.com>
2020-04-13 23:13:28 +0530

    Add solutions for java data structures

---
+++ hackerrank/src/main/java/lk/avix/Practice/Java/DataStructures/JavaList.java
@@ -0,0 +1,32 @@
+package lk.avix.Practice.Java.DataStructures;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(scanner.next()));
+        }
+
+        n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            String operator = scanner.next();
+            int index = Integer.parseInt(scanner.next());
+            if (operator.equals("Insert")) {
+                int value = Integer.parseInt(scanner.next());
+                list.add(index, value);
+            } else {
+                list.remove(index);
+            }
+        }
+
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+    }
+}
