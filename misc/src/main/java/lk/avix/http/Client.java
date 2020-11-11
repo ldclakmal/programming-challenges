9340a1a26a3cacf07e4465ef0784c111f757a30f
Chanaka Lakmal <ldclakmal@gmail.com>
2020-11-11 20:03:34 +0530

    Update client to support SSL

--- misc/src/main/java/lk/avix/http/Client.java
+++ misc/src/main/java/lk/avix/http/Client.java
@@ -1,22 +1,43 @@
 package lk.avix.http;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 public class Client {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    private static final String truststore = "ballerina-truststore.p12";
+    private static final String password = "ballerina";
+
+    public static void main(String[] args) throws Exception {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        String path = new File(Client.class.getClassLoader().getResource(truststore).getFile()).getAbsolutePath();
+        InputStream is = new FileInputStream(new File(path));
+        char[] passphrase = password.toCharArray();
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        ks.load(is, passphrase);
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(ks, passphrase);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        tmf.init(ks);
+        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+
         HttpClient client = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .connectTimeout(Duration.ofSeconds(5))
+                .sslContext(sslContext)
                 .build();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://asb0zigfg2.execute-api.us-west-2.amazonaws.com/v1/jwks"))
+                .uri(URI.create("https://www.googleapis.com/oauth2/v3/certs"))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println("Status Code: " + response.statusCode());
