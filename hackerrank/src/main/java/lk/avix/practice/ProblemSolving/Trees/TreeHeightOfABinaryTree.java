75977098607aad8c94e96f009dbd6d8949a0e640
Chanaka Lakmal <ldclakmal@gmail.com>
2020-12-29 10:11:23 +0530

    Add binary tree solutions

---
+++ hackerrank/src/main/java/lk/avix/practice/ProblemSolving/Trees/TreeHeightOfABinaryTree.java
@@ -0,0 +1,66 @@
+package lk.avix.practice.ProblemSolving.Trees;
+
+import java.util.Scanner;
+
+
+
+class TreeHeightOfABinaryTree {
+
+    // Code is Given
+    private static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    /*
+    class Node
+        int data;
+        Node left;
+        Node right;
+    */
+    public static int height(Node root) {
+        // Write your code here.
+        if (root == null) {
+            return -1;
+        }
+        return Math.max(height(root.left) + 1, height(root.right) + 1);
+    }
+
+    // Code is Given
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    // Code is Given
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }
+}
---
+++ hackerrank/src/main/java/lk/avix/practice/ProblemSolving/Trees/TreeLevelOrderTraversal.java
@@ -0,0 +1,73 @@
+package lk.avix.practice.ProblemSolving.Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class TreeLevelOrderTraversal {
+
+    // Code is Given
+    private static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    /*
+    class Node
+        int data;
+        Node left;
+        Node right;
+    */
+    public static void levelOrder(Node root) {
+        // Write your code here.
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node temp = queue.poll();
+            System.out.print(temp.data + " ");
+            if (temp.left != null) {
+                queue.add(temp.left);
+            }
+            if (temp.right != null) {
+                queue.add(temp.right);
+            }
+        }
+    }
+
+    // Code is Given
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    // Code is Given
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }
+}
