0a6d3fcedd0edc0a93a4624bf4cec07f85a5af99
Chanaka Lakmal <ldclakmal@gmail.com>
2021-03-03 07:56:38 +0530

    Update client source code

--- misc/src/main/java/lk/avix/http/Client.java
+++ misc/src/main/java/lk/avix/http/Client.java
@@ -6,42 +6,74 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Objects;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 public class Client {
 
-    private static final String truststore = "ballerina-truststore.p12";
+    private static final boolean sslDisable = false;
+    private static final String truststore = "ballerinaTruststore.p12";
     private static final String password = "ballerina";
 
     public static void main(String[] args) throws Exception {
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        String file = Objects.requireNonNull(Client.class.getClassLoader().getResource(truststore)).getFile();
-        String path = new File(file).getAbsolutePath();
-        FileInputStream is = new FileInputStream(path);
-        char[] passphrase = password.toCharArray();
-        KeyStore ks = KeyStore.getInstance("PKCS12");
-        ks.load(is, passphrase);
-
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        kmf.init(ks, passphrase);
-        TrustManagerFactory tmf = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        tmf.init(ks);
-        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
-
+        SSLContext sslContext;
+        if (sslDisable) {
+            sslContext = initSslContext();
+        } else {
+            String file = Objects.requireNonNull(Client.class.getClassLoader().getResource(truststore)).getFile();
+            String path = new File(file).getAbsolutePath();
+            sslContext = initSslContext(path, password);
+        }
         HttpClient client = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .sslContext(sslContext)
                 .build();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.googleapis.com/oauth2/v3/certs"))
+                .uri(URI.create("https://localhost:9090/oauth2/jwks"))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println("Status Code: " + response.statusCode());
         System.out.println("Response Body: " + response.body());
+    }
+
+    private static SSLContext initSslContext() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+        return sslContext;
+    }
+
+    private static SSLContext initSslContext(String path, String password) throws Exception {
+        try (FileInputStream is = new FileInputStream(path)) {
+            char[] passphrase = password.toCharArray();
+            KeyStore ks = KeyStore.getInstance("PKCS12");
+            ks.load(is, passphrase);
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            tmf.init(ks);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
+            return sslContext;
+        }
     }
 }
